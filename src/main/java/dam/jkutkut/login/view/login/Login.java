70614f76bf82c1a29992c9c35e04de97c6012c12
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-11 15:32:22 +0200

    working on login. Basic elements added

---
+++ src/main/java/dam/jkutkut/login/view/login/Login.java
@@ -0,0 +1,13 @@
+package dam.jkutkut.login.view.login;
+
+import javax.swing.*;
+
+public class Login {
+    private JPanel jpBody;
+    private JPanel jpForm;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPanel jpButtons;
+    private JButton button1;
+    private JButton button2;
+}
