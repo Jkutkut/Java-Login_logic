df6fe657e821e59a15dab665590e6d3872521363
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-11 15:44:34 +0200

    working on signup UI

---
+++ src/main/java/dam/jkutkut/login/view/signup/SignUp.java
@@ -0,0 +1,12 @@
+package dam.jkutkut.login.view.signup;
+
+import javax.swing.*;
+
+public class SignUp {
+    private JPanel jpForm;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton button1;
+    private JButton button2;
+}
