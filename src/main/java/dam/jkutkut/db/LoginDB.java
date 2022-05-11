0f72e93be8c8ad705920b208f695bbba870725ad
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-11 17:00:32 +0200

    now using better name

--- src/main/java/dam/jkutkut/db/LoginDB.java
+++ src/main/java/dam/jkutkut/db/LoginDB.java
@@ -1,13 +1,13 @@
 package dam.jkutkut.db;
 
-public class UserDB extends AccessDB {
+public class LoginDB extends AccessDB {
     private static final String DB_LOCATION = "db/loginDB.sqlite3";
 
     private static final String TABLE_NAME = "users";
     private static final String COLUMN_ID = "user";
     private static final String COLUMN_PASSWORD = "password";
 
-    public UserDB() {
+    public LoginDB() {
         super(DB_LOCATION);
     }
 }
